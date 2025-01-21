daa1a098021b35310e9c458381bf381bb972fd27
UED17_OlegAnkudinov <olegsip2002@gmail.com>
2025-01-21 21:47:01 +0500

    no message

--- vk_parser.py
+++ vk_parser.py
@@ -1,8 +1,4 @@
 # Ссылка для получения ключа: https://dev.vk.com/ru/api/access-token/getting-started
-# 
-# Ссылка для создания приложения для получения токена: https://dev.vk.com/ru
-# 
-# Статья о том как токен получать: https://smmplanner.com/blog/gaid-po-api-vk-kak-podkliuchit-i-ispolzovat/
 
 import os
 import json
@@ -28,14 +24,12 @@ def get_friends_ids(user_id):
         'access_token': os.getenv('VK_OAUTH_TOKEN')
     }
     response = requests.get('https://api.vk.com/method/' + method, params=payload).json()
-    #Check for any errors occuring
+
     if 'response' in response:
         uid2friends[user_id] = response['response']['items']
     else:
-        #print(response['error']['error_msg'])
         uid2friends[user_id] = []
     return uid2friends,len(uid2friends.keys())
-# age - такого параметра нет
 
 included_params= [
     'albums', #included
@@ -48,9 +42,6 @@ included_params= [
     'videos', #included
 ]
 
-
-# Parser Core
-# Получение даты регистрации аккаунта (спорный метод)
 def get_registration_date(id):
     url = f'https://vk.com/foaf.php?id={id}'
 
@@ -61,7 +52,6 @@ def get_registration_date(id):
                 return line.split('=')[1].split('T')[0].replace('"','')
     return None
 
-# Проверка на наличие папки для сохранения данных
 def ensure_directory_exists(directory_path):
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
@@ -72,7 +62,6 @@ def ensure_directory_exists(directory_path):
     return directory_path
     
 
-# сохранение данных в json
 def save_dict_to_json(data, filepath):
     with open(filepath, 'w', encoding='utf-8') as json_file:
         json.dump(data, json_file, indent=4,ensure_ascii=False)
@@ -81,7 +70,6 @@ def save_dict_to_json(data, filepath):
     return filepath
 
 
-# функция отправки запрсоа
 def send_request(method, params):
     url = "https://api.vk.com/method/" + method
 
@@ -92,7 +80,6 @@ def send_request(method, params):
         return data
     return None
 
-# функция получения информации о пользователе
 def get_user_info(user_id, access_token,user_params):
     method = "users.get"
  
@@ -115,7 +102,6 @@ def get_user_info(user_id, access_token,user_params):
         return None
         
 
-# функция получения информации о пользователе
 def get_user_friends(user_id, access_token):
     method = "friends.get"
  
@@ -135,32 +121,8 @@ def get_user_friends(user_id, access_token):
     else:
         return None
 
-# функция получения подписчиков пользователя
 def get_user_followers(user_id, access_token, result='amount', **followers_data):
-    """
-    Fetches the followers of a user from an API.
-
-    Args:
-        user_id (str): The ID or screen name of the user whose followers are to be fetched.
-        access_token (str): The access token for API authentication.
-        result (str, optional): Specifies the type of result to return. 
-                                Can be 'amount' to return the number of followers 
-                                or 'data' to return the full response data. 
-                                Defaults to 'amount'.
-        **followers_data: Additional parameters for fetching followers information.
-                          - get_followers_info (bool): If True, fetch additional follower info.
-                          - followers_params (str): Comma-separated list of fields to retrieve.
-                          - get_all_followers (bool): If True, fetch all followers.
-
-    Returns:
-        int or dict: 
-            - If result is 'amount', returns the number of followers as an integer.
-            - If result is 'data', returns the full response data as a dictionary.
-
-    Raises:
-        ValueError: If an invalid result type is specified.
-    """
-    
+   
     method = 'users.getFollowers'
 
     params = {
@@ -191,27 +153,7 @@ def get_user_followers(user_id, access_token, result='amount', **followers_data)
 
 # Функция получения подписок пользователя
 def get_user_subscriptions(user_id, access_token, result='amount', **subscriptions_data):
-    """
-    Fetches the subscription data for a user from an external API.
-
-    This function retrieves the number of followers (subscriptions) for a specified user
-    using the provided user ID and access token. It can return either the total count of 
-    followers or detailed subscription data based on the parameters provided.
-
-    Parameters:
-    - user_id (str or int): The unique identifier of the user whose subscriptions are to be fetched.
-    - access_token (str): The access token required for authentication with the API.
-    - result (str, optional): Specifies the type of result to return. 
-      - 'amount' (default): Returns the count of followers.
-      - 'data': Returns detailed subscription data.
-    - **subscriptions_data (dict, optional): Additional parameters for fetching subscription data.
-      - 'extended' (bool): If set to True, requests extended information about the subscriptions.
-      - 'amount_type' (str, optional): Specifies whether to return the count of 'users' or 'groups'.
-
-    Returns:
-    - int: If result is 'amount', returns the count of followers based on the specified amount_type.
-    - dict: If result is 'data', returns the complete response containing subscription details.
-    """
+   
     
     method = 'users.getSubscriptions'
 
@@ -240,7 +182,6 @@ def get_user_subscriptions(user_id, access_token, result='amount', **subscriptio
     elif result == 'data':
         return data['response']
 
-# функция получения данных
 def parse_data(user_name,user_params,save = True):
     token = os.getenv('VK_OAUTH_TOKEN')
     access_token = token  
@@ -258,19 +199,14 @@ def parse_data(user_name,user_params,save = True):
             save_dict_to_json(user_info, f'{filepath}/userinfo.json')
     return user_info
 
-
-# Create Table Module
-# Функция проверки значния
 def is_not_empty(value):
     if value not in ["", [], {}] and value is not None:
         return value
     else:
         return 0 
 
-
 def calculate_age(bdate: str):
-    '''Function for calculating age for birthday date in format 'day.month.year(4 digits)'   '''
-    #Birthday date is split to day, month, and year.
+
     bdate_list = bdate.split(".")
     if len(bdate_list) != 3:
         return None
@@ -320,7 +256,6 @@ def clear_data(user_info):
     return result
 
 
-# функция создания датафрейма
 def create_dataframe(user_info):
     result = clear_data(user_info)
     df = pd.DataFrame([result])
@@ -329,10 +264,7 @@ def create_dataframe(user_info):
     return df
 
 
-
-
 def parse_user_data(url):
-# Params
     user_params = [
         'activities',  # 'деятельность'
         'about',  # 'о себе'
@@ -387,5 +319,5 @@ def parse_user_data(url):
 
 
 if __name__ == "__main__":
-    user_url = input('Enter user url')
+    user_url = input('Введите url')
     print(parse_user_data(user_url))
